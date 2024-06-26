c1ef88aa9e31ae11333d62e54b8499adc4d8f314
AKSHAT YADAV <anshyadav56@gmail.com>
2024-06-26 16:50:48 +0530

    final

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,48 +1,13 @@
-import { ReactNode } from "react";
-import type { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
-import { Inter } from "next/font/google";
+import { ReactNode } from 'react';
 
-import "@stream-io/video-react-sdk/dist/css/styles.css";
-import "react-datepicker/dist/react-datepicker.css";
-import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
+import StreamVideoProvider from '@/providers/StreamClientProvider';
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "YOOM",
-  description: "Video calling App",
-  icons: {
-    icon: "/icons/logo.svg",
-  },
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: ReactNode }>) {
-  return (
-    <html lang="en">
-      <ClerkProvider
-        appearance={{
-          layout: {
-            socialButtonsVariant: "iconButton",
-            logoImageUrl: "/icons/yoom-logo.svg",
-          },
-          variables: {
-            colorText: "#fff",
-            colorPrimary: "#0E78F9",
-            colorBackground: "#1C1F2E",
-            colorInputBackground: "#252A41",
-            colorInputText: "#fff",
-          },
-        }}
-      >
-        <body className={`${inter.className} bg-dark-2`}>
-          <Toaster />
-          {children}
-        </body>
-      </ClerkProvider>
-    </html>
-  );
-}
+export default RootLayout;
